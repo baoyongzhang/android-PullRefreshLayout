aba205ee2db178f6fd8f15af1d3df7a43a2960a7
baoyongzhang <baoyz@live.com>
2015-09-24 21:56:22 +0800

    delete unless code

--- library/src/main/java/com/baoyz/widget/PullRefreshLayout.java
+++ library/src/main/java/com/baoyz/widget/PullRefreshLayout.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
